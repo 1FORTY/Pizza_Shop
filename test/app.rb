25f2ffb1131d38829116fe9067293f247945d847
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-10-11 21:50:31 +0700

    Add unit test for function

--- test/app.rb
+++ test/app.rb
@@ -1,1 +1,15 @@
-"product_1=11,product_2=4,product_3=11,"
+orders = "product_1=11,product_2=4,product_3=11,"
+
+s1 = orders.split(',')
+
+order = []
+s = []
+s1.each do |x|
+  s2 = x.split('=')[0].split('_')[1]
+  s3 = x.split('=')[1]
+
+  s = [s2, s3]
+  order += [s]
+end
+
+puts order.inspect
